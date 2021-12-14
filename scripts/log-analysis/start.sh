90785a568b2b2fe0cf16cff48ddccc34fd51d44a
octavianx <octavianx@stusci.com>
2021-12-14 14:57:14 +0800

    start.h   change the volumen mapping-> avoid samplelogs

--- scripts/log-analysis/start.sh
+++ scripts/log-analysis/start.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 docker run --rm -d -p "8888:8888" \
--v `pwd`/../../data/logs:/workspace/logs \
+-v `pwd`/../../data//minio/bucket:/workspace/localhost-bucket \
+-v `pwd`/../../data/logs:/workspace/localhost-logs \
 -v `pwd`/../../docker/volumes/.aws:/root/.aws \
 -v `pwd`/../../data/analysis:/workspace/analysis \
 --name loganalysis \
